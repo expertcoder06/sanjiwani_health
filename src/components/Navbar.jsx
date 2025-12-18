8d71bfa1d048fa8a0143d56a36cbeec74a48b9c0
RoshanKumar9421 <yadavroshankumar8271@gmail.com>
2025-12-18 23:32:24 +0530

    all updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,43 @@
-import './Navbar.css'
-import logo from '../assets/logo.png'
+import React from "react";
+import logo from "../assets/logo.jpeg";
+import { FiSearch } from "react-icons/fi";
 
-export default function Navbar() {
+const Navbar = () => {
   return (
-    <nav className="navbar">
-      <img src={logo} className="logo" />
+    <nav className="w-full h-[70px] flex items-center justify-between px-16">
+      
+      
+      <img
+        src={logo}
+        alt="Logo"
+        className="w-[70px] h-[70px] rounded-full border"
+      />
 
-      <ul className="nav-links">
-        <li>Home</li>
-        <li>My Appointments</li>
-        <li>Our Services</li>
+      
+      <ul className="flex items-center gap-8 text-[#035F69]">
+        <li className="cursor-pointer">Home</li>
+        <li className="cursor-pointer">My Appointments</li>
+
+        
+        <li className="flex items-center gap-8 cursor-pointer">
+          Our Service
+          <FiSearch className="text-lg" />
+        </li>
       </ul>
 
-      <div className="auth-buttons">
-        <button className="btn-outline">Sign In</button>
-        <button className="btn-filled">Sign Up</button>
+     
+      <div className="flex items-center gap-4">
+        <button className="w-[134px] h-[52px] rounded-[19px] bg-white text-[#035F69] border cursor-pointer">
+          Sign In
+        </button>
+
+        <button className="w-[138px] h-[53px] rounded-[19px] bg-[#035F69] text-white cursor-pointer">
+          Sign Up
+        </button>
       </div>
+
     </nav>
-  )
-}
+  );
+};
+
+export default Navbar;
